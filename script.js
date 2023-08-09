f961b901c2dfc4abb70bc2db1e8dedfe4bcee983
Luke Symon <lukealamil12@gmail.com>
2023-08-09 15:09:47 +0800

    update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const burgir = document.getElementById("hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-
 burgir.addEventListener("click", () => {
     burgir.classList.toggle("active");
     navMenu.classList.toggle("active");
