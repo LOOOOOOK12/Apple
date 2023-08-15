baad4037dbcefe9138da7885054dac88fcdecfc0
Luke Symon <lukealamil12@gmail.com>
2023-08-15 20:08:19 +0800

    update

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const burgir = document.getElementById("hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+let count = 0;
+
 burgir.addEventListener("click", () => {
     burgir.classList.toggle("active");
     navMenu.classList.toggle("active");
   })
+
+document.getElementById("increase-BTN").onclick = function(){
+    count += 1;
+    document.getElementById("count").innerHTML = count;
+}
+
+document.getElementById("decrease-BTN").onclick = function(){
+  count -= 1;
+  document.getElementById("count").innerHTML = count;
+}
+
+// function increase(){
+//     count += 1;
+//     document.querySelectorAll("#count").innerHTML = count;
+// }
