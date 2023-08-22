b985274f8105755b7810657fa4e29069fd2786d0
Luke Symon <lukealamil12@gmail.com>
2023-08-22 13:50:55 +0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const burgir = document.getElementById("hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+const purchase = document.getElementById("purchase-Button");
+
 let count = 0;
 
 burgir.addEventListener("click", () => {
@@ -8,17 +10,34 @@ burgir.addEventListener("click", () => {
     navMenu.classList.toggle("active");
   })
 
+//APPLE 1
 document.getElementById("increase-BTN").onclick = function(){
     count += 1;
     document.getElementById("count").innerHTML = count;
 }
 
 document.getElementById("decrease-BTN").onclick = function(){
-  count -= 1;
-  document.getElementById("count").innerHTML = count;
+  if(count == 0){
+      document.getElementById("decrease-BTN").disable = true;
+     
+  }
+  else{
+    count -= 1;
+    document.getElementById("count").innerHTML = count;
+  }
+  
 }
 
-// function increase(){
-//     count += 1;
-//     document.querySelectorAll("#count").innerHTML = count;
-// }
+purchase.onclick =() => {
+    if(count ===0 ){
+      alert("Amount empty! please get amount!");
+    }
+    else{
+      alert(`You have purchased this ${count} amount!`);
+    }
+   
+}
+
+
+
+
