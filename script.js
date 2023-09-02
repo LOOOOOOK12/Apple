9b8bb6dceb525c8a4b90de480b6937a0c5578d0e
Luke Symon <lukealamil12@gmail.com>
2023-09-02 15:58:23 +0800

    update

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
 const burgir = document.getElementById("hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-const purchase = document.getElementById("purchase-Button");
+const purchase = document.getElementsByClassName("purchase-Button");
 const increase = document.getElementsByClassName("increase-BTN");
+const decrease = document.getElementsByClassName("decrease-BTN");
 
 const counter = document.getElementsByClassName("count");
 
-let count = 0;
-
 let counts = [0, 0, 0, 0, 0, 0];
 
-burgir.addEventListener("click", () => {
-    burgir.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  })
-
-//APPLE 1
-
+// Increase (+) button
 for (var i = 0; i < increase.length; i++) {
   increase[i].addEventListener("click", function (index) {
-      return function () {
-          counts[index] += 1;
-          counter[index].innerHTML = counts[index];
-      };
+    return function () {
+      counts[index] += 1;
+      counter[index].innerHTML = counts[index];
+      updateCounterState(index); // Call the updateCounterState function
+    };
   }(i));
 }
 
-// increase[5].addEventListener("click", function () {
-//   count += 1;
-//   counter[5].innerHTML = count;
-// });
-
-// document.getElementById("decrease-BTN").onclick = function(){
-//   if(count == 0){
-//       document.getElementById("decrease-BTN").disable = true;
-     
-//   }
-//   else{
-//     count -= 1;
-//     document.getElementsByClassName("count").innerHTML = count;
-//   }
-  
-// }
-
-purchase.onclick =() => {
-    if(counts ===0 ){
-      alert("Amount empty! please get amount!");
-    }
-    else{
-      alert(`You have purchased this ${counts} amount!`);
-    }
-   
+// Decrease (-) button
+for (var i = 0; i < decrease.length; i++) {
+  decrease[i].addEventListener("click", function (index) {
+    return function () {
+      if (counts[index] > 0) {
+        counts[index] -= 1;
+        counter[index].innerHTML = counts[index];
+      }
+      updateCounterState(index); // Call the updateCounterState function
+    };
+  }(i));
 }
+
+function updateCounterState(index) {
+  if (counts[index] === 0) {
+    counter[index].setAttribute("disabled", "disabled"); // Fix the variable name here
+  } else {
+    counter[index].removeAttribute("disabled"); // Fix the variable name here
+  }
+}
+
+for (let i = 0; i < purchase.length; i++) {
+  purchase[i].addEventListener("click", function () {
+    const totalAmount = counts.reduce((acc, curr) => acc + curr, 0);
+
+    if (totalAmount === 0) {
+      alert("Amount empty! Please get amount!");
+    } else {
+      alert(`You have purchased a total of ${totalAmount} items!`);
+    }
+  });
+}
+
+
+burgir.addEventListener("click", () => {
+  burgir.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+
+
 
 
 
